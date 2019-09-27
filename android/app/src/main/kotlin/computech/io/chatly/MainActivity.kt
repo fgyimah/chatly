53175bb755aa8e919688580700d437a0d1e47180
Francis Gyimah <fgyimah883@gmail.com>
2019-09-27 23:32:17 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/computech/io/chatly/MainActivity.kt
@@ -0,0 +1,13 @@
+package computech.io.chatly
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,10 @@
+import 'package:chatly/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+}
